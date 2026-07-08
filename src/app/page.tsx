eccf87744a2c9f38f11d0a2b10aee612bc3c21b5
correia-henrique16 <correiahenrique8@gmail.com>
2026-07-08 17:15:56 +0100

    Botao para o Profile (sem ligação ainda, nem estilizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ProfileBtn from "@/components/inicial/ProfileBtn";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Tamos aí</h1>
+      <nav className="bg-amber-500 p-2 px-10">
+        <ProfileBtn />
+      </nav>
     </div>
   );
 }
---
+++ src/components/inicial/ProfileBtn.tsx
@@ -0,0 +1,14 @@
+import { getRequiredUser } from "@/lib/supabase/user"
+import Link from "next/link"
+
+export default async function ProfileBtn() {
+    const user = await getRequiredUser()
+    
+    const firtLetter = user.user_metadata.full_name.charAt(0).toUpperCase()
+
+    return(
+        <Link href="" className="rounded-full bg-blue-200 text-white w-12 h-12 centered-flex">
+            <span>{firtLetter}</span>
+        </Link>
+    )
+}
