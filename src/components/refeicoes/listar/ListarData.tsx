3163ac27a18edad759fdbfe91bebf08328d7fa21
correia-henrique16 <correiahenrique8@gmail.com>
2026-07-13 16:45:10 +0100

    Atualização na data para dizer 'hoje, amanha e ontem', e aparecer so dia e mes

--- src/components/refeicoes/listar/ListarData.tsx
+++ src/components/refeicoes/listar/ListarData.tsx
@@ -1,14 +1,33 @@
 import { Dispatch, SetStateAction } from "react"
+import datasDias from "@/utils/datas/datasDias"
 
 interface ChildProps {
-    date: string
+    date: string,
     setDate: Dispatch<SetStateAction<string>>
 }
 
 export default function ListarData({date, setDate}: ChildProps) {
+    const {anteriorDataString, dataSelecionada, proximaDataString, getLabel} = datasDias(date)  
+
     return (
         <section className="w-screen centered-flex">
-            <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+            <button onClick={() => setDate(anteriorDataString)} >
+                &lt;
+            </button>
+
+            <div className="flex justify-center items-center gap-4 w-2/3">
+                <p>{getLabel(anteriorDataString)}</p>
+
+                <p>{getLabel(dataSelecionada)}</p>
+
+                <p>{getLabel(proximaDataString)}</p>
+            </div>
+
+            <button onClick={() => setDate(proximaDataString)} >
+                &gt;
+            </button>
+
+            {/* <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/> */}
         </section>
     )
 }
---
+++ src/utils/datas/datasDias.ts
@@ -0,0 +1,30 @@
+export default function datasDias(date: string) {
+    const [ano, mes, dia] = date.split('-').map(Number)
+    const proximaData = new Date(ano, mes - 1, dia + 1)
+    const anteriorData = new Date(ano, mes - 1, dia - 1)
+    const dataSelecionada = date
+
+    const proximaDataString = `${proximaData.getFullYear()}-${String(proximaData.getMonth() + 1).padStart(2, '0')}-${String(proximaData.getDate()).padStart(2, '0')}`
+    const anteriorDataString = `${anteriorData.getFullYear()}-${String(anteriorData.getMonth() + 1).padStart(2, '0')}-${String(anteriorData.getDate()).padStart(2, '0')}`
+
+    const hoje = new Date()
+    const amanha = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate() + 1)
+    const ontem = new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate() - 1)
+
+    const hojeString = `${hoje.getFullYear()}-${String(hoje.getMonth() + 1).padStart(2, '0')}-${String(hoje.getDate()).padStart(2, '0')}`
+    const ontemString = `${ontem.getFullYear()}-${String(ontem.getMonth() + 1).padStart(2, '0')}-${String(ontem.getDate()).padStart(2, '0')}`
+    const amanhaString = `${amanha.getFullYear()}-${String(amanha.getMonth() + 1).padStart(2, '0')}-${String(amanha.getDate()).padStart(2, '0')}`
+
+    const getLabel = (strData: string) => {
+        if (strData === hojeString) return 'Hoje'
+        if (strData === ontemString) return 'Ontem'
+        if (strData === amanhaString) return 'Amanhã'
+
+        const [ano, mes, dia] = strData.split('-').map(Number)
+        return (`${dia}/${mes}`)
+    }
+
+    return {
+        dataSelecionada, proximaDataString, anteriorDataString, getLabel
+    }
+}
