c72184668e5622cd70316f7cfcb1d3f3ad350f81
correia-henrique16 <correiahenrique8@gmail.com>
2026-07-22 10:10:14 +0100

    Estilização Adicionar Alimentos

--- src/app/(dashboard)/alimentos/page.tsx
+++ src/app/(dashboard)/alimentos/page.tsx
@@ -3,9 +3,6 @@ import AddAlimentoForm from "@/components/alimentos/AddAlimentoForm"
 export default async function AlimentosPage() {
 
     return (
-        <main>
-            <h1>Adicionar Alimento</h1>
-            <AddAlimentoForm />
-        </main>
+            <AddAlimentoForm />            
     )
 }
