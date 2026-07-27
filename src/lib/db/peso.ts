03f8fdb612e0818065d107f3efac49c57c751731
correia-henrique16 <correiahenrique8@gmail.com>
2026-07-27 10:32:44 +0100

    api e back para adicionar peso

--- src/lib/db/peso.ts
+++ src/lib/db/peso.ts
@@ -1,6 +1,26 @@
 import { createClient } from "../supabase/server";
+import { TypePeso } from "@/models/input/Peso";
 import { getRequiredUser } from "../supabase/user";
 
+export async function addPeso(novoPeso: TypePeso) {
+    const user = await getRequiredUser()
+
+    const supabase = await createClient()
+    
+    const {data, error} = await supabase
+    .from('historico_peso')
+    .insert ([{
+        ...novoPeso,
+        user_id: user.id
+    }])
+    .select()
+
+    if (error) {
+        throw new Error(error.message)
+    } else {
+        return data
+    }
+}
 
 export async function getPesoAtual() {
     const supabase = await createClient()
