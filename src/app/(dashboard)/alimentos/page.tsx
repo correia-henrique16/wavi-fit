5076d3a07f88af587d77e379e7c80584a0d3623e
correia-henrique16 <correiahenrique8@gmail.com>
2026-07-30 12:09:47 +0100

    Suspense na page Alimentos por ter SearchParams na form (vercel)

--- src/app/(dashboard)/alimentos/page.tsx
+++ src/app/(dashboard)/alimentos/page.tsx
@@ -1,10 +1,14 @@
 import AddAlimentoForm from "@/components/alimentos/AddAlimentoForm"
+import { Suspense } from "react"
 
 export const dynamic = 'force-dynamic'
 
 export default async function AlimentosPage() {
 
     return (
-            <AddAlimentoForm />            
+        <Suspense fallback={<p>A carregar...</p>}>
+            <AddAlimentoForm />
+        </Suspense>
+                        
     )
 }
